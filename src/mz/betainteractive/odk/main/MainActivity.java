b742e1a10e054827c48424e588c02bf9993b9567
Paulo Filimone <paulphilimone@gmail.com>
2017-08-01 12:49:51 +0200

    Loading ODK Form using FormUtilities class

--- src/mz/betainteractive/odk/main/MainActivity.java
+++ src/mz/betainteractive/odk/main/MainActivity.java
@@ -3,7 +3,11 @@ package mz.betainteractive.odk.main;
 
 
 
+import java.io.File;
+
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -11,13 +15,17 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import mz.betainteractive.odk.FormUtilities;
 import mz.betainteractive.odk.R;
+import mz.betainteractive.odk.listener.OdkFormResultListener;
 import mz.betainteractive.odk.model.FilledForm;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OdkFormResultListener {
 	
 	private EditText txtFormId;
 	private Button btOpenODK;	
+	
+	private FormUtilities formUtilities;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +43,9 @@ public class MainActivity extends Activity {
 	}
 	
 	private void initComponents() {
+		
+		this.formUtilities = new FormUtilities(this);
+		
 		txtFormId = (EditText) findViewById(R.id.txtFormId);		
 		btOpenODK = (Button) findViewById(R.id.btOpenODK);
 		
@@ -51,16 +62,39 @@ public class MainActivity extends Activity {
 	protected void onClickedOpenODK() {
 		Log.d("loading", "click");
 						
-		String formName = txtFormId.getText().toString();
+		String formId = txtFormId.getText().toString();
 		
 		
-		FilledForm filledForm = new FilledForm(formName);
+		FilledForm filledForm = new FilledForm(formId);
 		
 		filledForm.put("visitId","MAN00000333");
 		filledForm.put("locationId", "MAN000001");
 		filledForm.put("roundNumber", 16);			
 				
-		//loadForm(1, filledForm);
+		formUtilities.loadForm(filledForm);
+	}
+	
+	/* Overriding ActivityResult and pass the results to FormUtilities ActivityResult*/
+	@Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        formUtilities.onActivityResult(requestCode, resultCode, data, this);
+    }
+
+	@Override
+	public void onFormFinalized(Uri contentUri, File xmlFile) {
+		 Log.d("form finalized"," "+contentUri+", "+xmlFile);		
+	}
+
+	@Override
+	public void onFormUnFinalized(Uri contentUri, File xmlFile) {
+		Log.d("form unfinalized"," "+contentUri);
+	}
+
+	@Override
+	public void onDeleteForm(Uri contentUri) {
+		Log.d("delete uri", "needs to be implemented");
 	}
     
     
